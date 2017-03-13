bd5fb9fe32c58ce5c1f36f425011c12bc129fb9b
Andrew Hearin <andrew.hearin@yale.edu>
2017-03-13 19:07:36 -0400

    Initial layout for package

---
+++ segmass/__init__.py
@@ -0,0 +1,10 @@
+"""
+"""
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    # from .example_mod import *
+    pass
