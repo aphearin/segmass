fa6c5e18aeccde69d58b5a08d8d610e41f0f83d0
Andrew Hearin <andrew.hearin@yale.edu>
2017-05-30 11:29:24 -0700

    Added logmpeak_threshold kwarg

--- segmass/load_baseline_mock.py
+++ segmass/load_baseline_mock.py
@@ -1,13 +1,18 @@
 """
 """
+import numpy as np
 from halotools.sim_manager import CachedHaloCatalog
 from halotools.empirical_models import PrebuiltSubhaloModelFactory
 
 
-def load_behroozi10_mock(simname='bolplanck', redshift=0, logsm_threshold=10):
+def load_behroozi10_mock(simname='bolplanck', redshift=0.,
+        logsm_threshold=10, logmpeak_threshold=-np.inf):
+    """
+    """
     halocat = CachedHaloCatalog(simname=simname, redshift=redshift)
     model = PrebuiltSubhaloModelFactory('behroozi10', redshift=redshift)
     model.populate_mock(halocat)
 
-    mask = model.mock.galaxy_table['stellar_mass'] > 10**logsm_threshold
+    mask = model.mock.galaxy_table['stellar_mass'] >= 10**logsm_threshold
+    mask *= model.mock.galaxy_table['halo_mpeak'] >= 10**logmpeak_threshold
     return model.mock.galaxy_table[mask]
