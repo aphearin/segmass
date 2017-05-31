42eaae418be57dd46e5ea75f6470c04cc2ed9f90
Andrew Hearin <andrew.hearin@yale.edu>
2017-05-30 20:30:42 -0700

    Added untested implementation of assign_posvel

--- segmass/remapped_radial_positions.py
+++ segmass/remapped_radial_positions.py
@@ -2,6 +2,7 @@
 """
 import numpy as np
 from halotools.empirical_models.phase_space_models.analytic_models.satellites.nfw.kernels.mc_generate_nfw_radial_positions import mc_generate_nfw_radial_positions
+from halotools.empirical_models.phase_space_models.analytic_models.satellites.nfw.kernels.biased_isotropic_velocity import dimensionless_radial_velocity_dispersion
 
 
 def digitize_concentration(conc, conc_bin_edges):
@@ -44,3 +45,58 @@ def assign_r_by_rvir(galaxy_concentration, conc_bin_edges):
                 num_pts=num_gals_iconc, conc=approx_conc, halo_radius=1.)
 
     return r_by_rvir
+
+
+def assign_posvel(halo_conc, gal_conc, conc_bin_edges):
+    """
+    Examples
+    --------
+    >>> num_gals = int(1e4)
+    >>> cmin, cmax = 2, 25
+    >>> halo_conc = np.random.uniform(cmin, cmax, num_gals)
+    >>> gal_conc = np.random.uniform(0.5, 2, num_gals)*halo_conc
+    >>> conc_bin_edges = np.linspace(cmin, cmax, 5)
+    >>> r_by_rvir, vdisp = assign_posvel(halo_conc, gal_conc, conc_bin_edges)
+    """
+
+    r_by_rvir = np.zeros_like(halo_conc)
+    vdisp = np.zeros_like(halo_conc)
+
+    idx_gal_conc, conc_bin_midpoints = digitize_concentration(gal_conc, conc_bin_edges)
+    idx_halo_conc, conc_bin_midpoints = digitize_concentration(halo_conc, conc_bin_edges)
+    num_bin_midpoints = len(conc_bin_midpoints)
+
+    #  Loop over galaxy concentrations
+    for i in range(num_bin_midpoints):
+        approx_gal_conc = conc_bin_midpoints[i]
+
+        #  Identify all objects with gal_conc in bin i
+        idx_gal_conc_mask = idx_gal_conc == i
+
+        num_gals_ibin = np.count_nonzero(idx_gal_conc_mask)
+
+        if num_gals_ibin > 0:
+            #  For all objects with gal_conc in bin i, calculate r/Rvir
+            r_by_rvir_ibin = mc_generate_nfw_radial_positions(
+                    num_pts=num_gals_ibin, conc=approx_gal_conc, halo_radius=1.)
+            r_by_rvir[idx_gal_conc_mask] = r_by_rvir_ibin
+
+            vdisp_ibin = np.zeros(num_gals_ibin)
+            #  Loop over halo concentrations
+            for j in range(num_bin_midpoints):
+                approx_halo_conc = conc_bin_midpoints[j]
+
+                #  Identify all objects with gal_conc in bin i
+                idx_halo_conc_mask = idx_halo_conc[idx_gal_conc_mask] == j
+
+                num_gals_ijbin = np.count_nonzero(idx_halo_conc_mask)
+                if num_gals_ijbin > 0:
+                    r_by_rvir_ijbin = r_by_rvir_ibin[idx_halo_conc_mask]
+                    vdisp_ijbin = dimensionless_radial_velocity_dispersion(
+                        r_by_rvir_ijbin, approx_halo_conc, approx_gal_conc)
+                    vdisp_ibin[idx_halo_conc_mask] = vdisp_ijbin
+
+            vdisp[idx_gal_conc_mask] = vdisp_ibin
+
+    return r_by_rvir, vdisp
+
